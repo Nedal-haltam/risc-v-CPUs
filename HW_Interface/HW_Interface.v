4b75249e1c438e7d9e7a4e629c59ce9e88a1690b
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-11-15 01:13:51 +0300

    implement a vector add that does DMA to transfer data

--- HW_Interface/HW_Interface.v
+++ HW_Interface/HW_Interface.v
@@ -22,6 +22,19 @@
 `define MMIO_ALU_done_rden  (ControlBus[1] && (`CPU_AddressBus == (16384 - (6 * 8))))
 `define MMIO_ALU_done_wren  (ControlBus[2] && (`CPU_AddressBus == (16384 - (6 * 8))))
 
+`define MMIO_VADD_addr_in1_rden (ControlBus[1] && (`CPU_AddressBus == (16384 - (7 * 8))))
+`define MMIO_VADD_addr_in1_wren (ControlBus[2] && (`CPU_AddressBus == (16384 - (7 * 8))))
+`define MMIO_VADD_addr_in2_rden (ControlBus[1] && (`CPU_AddressBus == (16384 - (8 * 8))))
+`define MMIO_VADD_addr_in2_wren (ControlBus[2] && (`CPU_AddressBus == (16384 - (8 * 8))))
+`define MMIO_VADD_addr_out_rden (ControlBus[1] && (`CPU_AddressBus == (16384 - (9 * 8))))
+`define MMIO_VADD_addr_out_wren (ControlBus[2] && (`CPU_AddressBus == (16384 - (9 * 8))))
+`define MMIO_VADD_vsize_rden    (ControlBus[1] && (`CPU_AddressBus == (16384 - (10 * 8))))
+`define MMIO_VADD_vsize_wren    (ControlBus[2] && (`CPU_AddressBus == (16384 - (10 * 8))))
+`define MMIO_VADD_start_rden    (ControlBus[1] && (`CPU_AddressBus == (16384 - (11 * 8))))
+`define MMIO_VADD_start_wren    (ControlBus[2] && (`CPU_AddressBus == (16384 - (11 * 8))))
+`define MMIO_VADD_done_rden     (ControlBus[1] && (`CPU_AddressBus == (16384 - (12 * 8))))
+`define MMIO_VADD_done_wren     (ControlBus[2] && (`CPU_AddressBus == (16384 - (12 * 8))))
+
 module HW_Interface(
 
 	//////////// CLOCK //////////
@@ -56,8 +69,30 @@ always@(posedge ADC_CLK_10) begin
 	ClockDivider <= ClockDivider + 1'b1;
 end
 
+wire clk;
+wire rst;
+wire cpu_clk;
+
+wire exit_ecall;
+wire write_ecall_finished;
+wire write_ecall;
+wire `BIT_WIDTH write_ecall_fd;
+wire `BIT_WIDTH write_ecall_address;
+wire `BIT_WIDTH write_ecall_len;
+wire datatrigger;
+wire [(`DM_BITS-1):0] write_ecall_mem_addr;
+
+wire `BIT_WIDTH AddressBus1, DMDataBusPort1, CPUDataBusIn, DMDataBusPort2, CPUDataBusOut;
+wire [10:0] ControlBus;
+wire `BIT_WIDTH CyclesConsumed;
+
 reg `BIT_WIDTH offset;
 reg done;
+
+wire clkPort2;
+wire rdenPort2, wrenPort2;
+wire [(`DM_BITS - 1):0] addressPort2;
+wire `BIT_WIDTH dataPort2;
 
 // MMIO
 //------------------------------------------------
@@ -72,30 +107,41 @@ reg `BIT_WIDTH alu_in1, alu_in2, alu_out;
 reg `BIT_WIDTH alu_start;
 reg `BIT_WIDTH alu_done;
 
-reg alu_done_set;
 wire alu_start_set;
+reg alu_done_set;
 wire alu_done_rden;
 parameter [3:0] ALU_STATE_OFF  = 4'd1;
 parameter [3:0] ALU_STATE_ON   = 4'd2;
-reg `BIT_WIDTH alu_counter;
 reg [3:0] alu_state;
+reg `BIT_WIDTH alu_counter;
+//------------------------------------------------
+//------------------------------------------------
+// mm_vadd
+reg `BIT_WIDTH vadd_addr_in1, vadd_addr_in2, vadd_addr_out;
+reg `BIT_WIDTH vadd_vsize;
+reg `BIT_WIDTH vadd_start, vadd_done;
 
-wire clk;
-wire rst;
-wire cpu_clk;
+reg vadd_dm_p2_en;
+reg [(`DM_BITS-1):0] vadd_dm_p2_address;
+reg vadd_dm_p2_wren;
+reg `BIT_WIDTH vadd_dm_p2_writedata;
+wire vadd_start_set;
+reg vadd_done_set;
+wire vadd_done_rden;
+parameter [3:0] VADD_STATE_OFF        = 4'd1;
+parameter [3:0] VADD_STATE_ON         = 4'd2;
+parameter [3:0] VADD_STATE_SET_ADDR_A = 4'd3;
+parameter [3:0] VADD_STATE_READ_A     = 4'd4;
+parameter [3:0] VADD_STATE_SET_ADDR_B = 4'd5;
+parameter [3:0] VADD_STATE_READ_B     = 4'd6;
+parameter [3:0] VADD_STATE_OP         = 4'd7;
+parameter [3:0] VADD_STATE_WRITE_C    = 4'd8;
+parameter [3:0] VADD_STATE_INC_OFFSET = 4'd9;
+reg [3:0] vadd_state;
+reg `BIT_WIDTH vadd_rega, vadd_regb, vadd_regc;
+reg `BIT_WIDTH vadd_offset;
+//------------------------------------------------
 
-wire exit_ecall;
-wire write_ecall_finished;
-wire write_ecall;
-wire `BIT_WIDTH write_ecall_fd;
-wire `BIT_WIDTH write_ecall_address;
-wire `BIT_WIDTH write_ecall_len;
-wire datatrigger;
-wire [(`DM_BITS-1):0] write_ecall_mem_addr;
-
-wire `BIT_WIDTH AddressBus1, DMDataBus, CPUDataBusIn, write_DMDataBus, CPUDataBusOut;
-wire [10:0] ControlBus;
-wire `BIT_WIDTH CyclesConsumed;
 
 always@(negedge clk or posedge rst) begin
     if (rst) begin
@@ -145,14 +191,15 @@ dualpram dualpram_inst
 	.wren_a(`DataMem_wren),
 	.address_a(`CPU_AddressBus),
 	.data_a(CPUDataBusOut),
-	.q_a(DMDataBus),
+	.q_a(DMDataBusPort1),
 
-	.clock_b(clk),
-	.rden_b(!done),
-	.wren_b(1'b0),
-	.address_b(write_ecall_mem_addr),
-	.data_b(0),
-	.q_b(write_DMDataBus)
+
+	.clock_b(clkPort2),
+	.rden_b(rdenPort2),
+	.wren_b(wrenPort2),
+	.address_b(addressPort2),
+	.data_b(dataPort2),
+	.q_b(DMDataBusPort2)
 );
 
 // MMIO control circuit
@@ -184,6 +231,26 @@ always@(negedge clk or posedge rst) begin
 			MMIODataBus <= alu_done;
 		end
 		//------------------------------------------------
+		// mm_vadd
+		else if (`MMIO_VADD_addr_in1_rden) begin
+			MMIODataBus <= vadd_addr_in1;
+		end
+		else if (`MMIO_VADD_addr_in2_rden) begin
+			MMIODataBus <= vadd_addr_in2;
+		end
+		else if (`MMIO_VADD_addr_out_rden) begin
+			MMIODataBus <= vadd_addr_out;
+		end
+		else if (`MMIO_VADD_vsize_rden) begin
+			MMIODataBus <= vadd_vsize;
+		end
+		else if (`MMIO_VADD_start_rden) begin
+			MMIODataBus <= vadd_start;
+		end
+		else if (`MMIO_VADD_done_rden) begin
+			MMIODataBus <= vadd_done;
+		end
+		//------------------------------------------------
 		// add other MM regs to read from
 	end
 	else if (`MMIO_wren) begin
@@ -199,6 +266,20 @@ always@(negedge clk or posedge rst) begin
 		end
 		else if (`MMIO_ALU_in2_wren)begin
 			alu_in2 <= CPUDataBusOut;
+		end
+		//------------------------------------------------
+		// mm_vadd
+		else if (`MMIO_VADD_addr_in1_wren) begin
+			vadd_addr_in1 <= CPUDataBusOut;
+		end
+		else if (`MMIO_VADD_addr_in2_wren) begin
+			vadd_addr_in2 <= CPUDataBusOut;
+		end
+		else if (`MMIO_VADD_addr_out_wren) begin
+			vadd_addr_out <= CPUDataBusOut;
+		end
+		else if (`MMIO_VADD_vsize_wren) begin
+			vadd_vsize <= CPUDataBusOut;
 		end
 		//------------------------------------------------
 		// add other MM regs to write to
@@ -264,29 +345,121 @@ assign alu_done_rden = `MMIO_ALU_done_rden;
 assign alu_start_set = `MMIO_ALU_start_wren && (CPUDataBusOut == 64'd1);
 //----------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------
+// vadd_start: reset after set
+always@(posedge clk or posedge rst) begin
+	if (rst) begin
+		vadd_start <= 0;
+	end
+	else if (vadd_start_set) begin
+		vadd_start <= 64'd1;
+	end
+	else if (vadd_start == 64'd1) begin
+		vadd_start <= 0;
+	end
+end
 
+// vadd_done : reset after reading
+always@(posedge clk or posedge rst) begin
+	if (rst) begin
+		vadd_done <= 0;
+	end
+	else if (vadd_done_set) begin
+		vadd_done <= 64'd1;
+	end
+	else if (vadd_done_rden) begin
+		vadd_done <= 0;
+	end
+end
+
+always@(posedge clk or posedge rst) begin
+	if (rst) begin
+		vadd_state <= VADD_STATE_OFF;
+		vadd_dm_p2_en <= 0;
+		vadd_done_set <= 0;
+		vadd_offset <= 0;
+		vadd_dm_p2_wren <= 0;
+	end
+	else begin
+		case(vadd_state)
+			VADD_STATE_OFF: begin
+				vadd_done_set <= 0;
+				vadd_dm_p2_en <= 0;
+				vadd_offset <= 0;
+				vadd_dm_p2_wren <= 0;
+				if (vadd_start == 64'd1) begin
+					vadd_state <= VADD_STATE_ON;
+				end
+			end
+			VADD_STATE_ON: begin
+				if (vadd_offset < vadd_vsize) begin
+					vadd_state <= VADD_STATE_SET_ADDR_A;
+					vadd_dm_p2_en <= 1'b1;
+				end
+				else begin
+					vadd_done_set <= 1'b1;
+					vadd_dm_p2_en <= 1'b0;
+					vadd_state <= VADD_STATE_OFF;
+				end
+			end
+			VADD_STATE_SET_ADDR_A: begin
+				vadd_dm_p2_address <= vadd_addr_in1 + (64'd8 * vadd_offset);
+				vadd_state <= VADD_STATE_READ_A;
+			end
+			VADD_STATE_READ_A: begin
+				vadd_rega <= DMDataBusPort2;
+				vadd_state <= VADD_STATE_SET_ADDR_B;
+			end
+			VADD_STATE_SET_ADDR_B: begin
+				vadd_dm_p2_address <= vadd_addr_in2 + (64'd8 * vadd_offset);
+				vadd_state <= VADD_STATE_READ_B;
+			end
+			VADD_STATE_READ_B: begin
+				vadd_regb <= DMDataBusPort2;
+				vadd_state <= VADD_STATE_OP;
+			end
+			VADD_STATE_OP: begin
+				vadd_regc <= vadd_rega + vadd_regb;
+				vadd_state <= VADD_STATE_WRITE_C;
+			end
+			VADD_STATE_WRITE_C: begin
+				vadd_dm_p2_address <= vadd_addr_out + (64'd8 * vadd_offset);
+				vadd_dm_p2_wren <= 1'b1;
+				vadd_dm_p2_writedata <= vadd_regc;
+				vadd_state <= VADD_STATE_INC_OFFSET;
+			end
+			VADD_STATE_INC_OFFSET: begin
+				vadd_dm_p2_wren <= 1'b0;
+				vadd_offset <= vadd_offset + 1'b1;
+				vadd_state <= VADD_STATE_ON;
+			end
+		endcase
+	end
+end
+assign vadd_done_rden = `MMIO_VADD_done_rden;
+assign vadd_start_set = `MMIO_VADD_start_wren && (CPUDataBusOut == 64'd1);
 //----------------------------------------------------------------------------------------
 
+assign clkPort2     = (vadd_dm_p2_en) ? ~clk   : clk;
+assign rdenPort2    = (vadd_dm_p2_en) ? (~vadd_dm_p2_wren) : !done;
+assign wrenPort2    = (vadd_dm_p2_en) ? (vadd_dm_p2_wren) : 1'b0;
+assign addressPort2 = (vadd_dm_p2_en) ? (vadd_dm_p2_address) : write_ecall_mem_addr;
+assign dataPort2    = (vadd_dm_p2_en) ? (vadd_dm_p2_writedata)    : 0;
 
-// bcd7seg HEX0_DISP
-// (
-// 	.num(write_ecall_len[3:0]),
-// 	.display(HEX0)
-// );
+assign clk = ClockDivider[9];
+assign rst = ~KEY[0];
 
+//-------------------
 assign datatrigger = (!done) ? (rst | ~clk) : (datatrigger);
-assign ARDUINO_IO[7:0] = (!done && (offset) <= (write_ecall_len)) ? write_DMDataBus[7:0] : 8'd0;
+assign ARDUINO_IO[7:0] = (!done && (offset) <= (write_ecall_len)) ? DMDataBusPort2[7:0] : 8'd0;
 assign ARDUINO_IO[8] = datatrigger;
 assign ARDUINO_RESET_N = 1'b1;
 assign write_ecall_mem_addr = write_ecall_address[(`DM_BITS-1):0] + offset[(`DM_BITS-1):0];
 assign write_ecall_finished = done;
-// TODO: show an idnication that the FPGA finished correctly through the 7seg, leds, exit code, ...
-assign clk = ClockDivider[9];
-assign rst = ~KEY[0];
+//-------------------
+assign CPUDataBusIn = (`DataMem_rden) ? DMDataBusPort1 : MMIODataBus;
 
-assign CPUDataBusIn = (`DataMem_rden) ? DMDataBus : MMIODataBus;
+
 assign LEDR = LED_MM_REG[9:0];
-
 assign HEX0[0] = clk;
 assign HEX0[1] = cpu_clk;
 assign HEX0[2] = rst;
@@ -296,6 +469,10 @@ assign HEX0[5] = write_ecall_finished;
 assign HEX0[6] = write_ecall;
 assign HEX0[7] = exit_ecall;
 
-
 endmodule
 
+// bcd7seg HEX0_DISP
+// (
+// 	.num(write_ecall_len[3:0]),
+// 	.display(HEX0)
+// );
--- singlecycle/test/test.e
+++ singlecycle/test/test.e
@@ -1,10 +1,18 @@
 
-#define LED (16384 - 8)
+#define LED (16384 - (1 * 8))
+
 #define alu_in1   (16384 - (2 * 8))
 #define alu_in2   (16384 - (3 * 8))
 #define alu_out   (16384 - (4 * 8))
 #define alu_start (16384 - (5 * 8))
 #define alu_done  (16384 - (6 * 8))
+
+#define vadd_addr_in1 (16384 - (7 * 8))
+#define vadd_addr_in2 (16384 - (8 * 8))
+#define vadd_addr_out (16384 - (9 * 8))
+#define vadd_vsize    (16384 - (10 * 8))
+#define vadd_start    (16384 - (11 * 8))
+#define vadd_done     (16384 - (12 * 8))
 
 func mmread(auto address)
 {
@@ -15,7 +23,7 @@ func mmwrite(auto address, auto value)
     *address = value;
 }
 
-func leds_counter_loop()
+func mm_leds_counter_loop()
 {
     auto counter = 0;
     while(1)
@@ -39,8 +47,9 @@ func leds_counter_loop()
     }
 }
 
-func main()
+func mm_alu_add()
 {
+    print("--------------MM ALU example started--------------\n");
     mmwrite(alu_in1, 123);
     print("wrote on alu_in1\n");
 
@@ -55,7 +64,44 @@ func main()
     print("it is DONE!\n");
 
     print("alu_out = %d\n", mmread(alu_out));
+    print("--------------------------------------------------\n");
+}
+#define VSIZE 10
+func mm_vadd()
+{
+    print("---------------------MM vector add example started---------------------\n");
+    auto xs[VSIZE];
+    auto ys[VSIZE];
+    auto zs[VSIZE];
 
+    for (auto i = 0; i < VSIZE; i += 1) xs[i] = 3 * (i + 1) + 1;
+    for (auto i = 0; i < VSIZE; i += 1) ys[i] = 4 * (i + 1) + 1;
+    for (auto i = 0; i < VSIZE; i += 1) zs[i] = 0;
+
+    mmwrite(vadd_addr_in1, xs);
+    mmwrite(vadd_addr_in2, ys);
+    mmwrite(vadd_addr_out, zs);
+    mmwrite(vadd_vsize, VSIZE);
+
+    print("xs mmread(%d), (%d): ", mmread(vadd_addr_in1), xs); for (auto i = 0; i < VSIZE; i += 1) print("%d ", xs[i]); print("\n");
+    print("ys mmread(%d), (%d): ", mmread(vadd_addr_in2), ys); for (auto i = 0; i < VSIZE; i += 1) print("%d ", ys[i]); print("\n");
+    print("zs mmread(%d), (%d): ", mmread(vadd_addr_out), zs); for (auto i = 0; i < VSIZE; i += 1) print("%d ", zs[i]); print("\n");
+
+    mmwrite(vadd_start, 1);
+    while(!mmread(vadd_done));
+
+    print("it is DONE!\n");
+
+    print("xs mmread(%d), (%d): ", mmread(vadd_addr_in1), xs); for (auto i = 0; i < VSIZE; i += 1) print("%d ", xs[i]); print("\n");
+    print("ys mmread(%d), (%d): ", mmread(vadd_addr_in2), ys); for (auto i = 0; i < VSIZE; i += 1) print("%d ", ys[i]); print("\n");
+    print("zs mmread(%d), (%d): ", mmread(vadd_addr_out), zs); for (auto i = 0; i < VSIZE; i += 1) print("%d ", zs[i]); print("\n");
+
+    print("-----------------------------------------------------------------------\n");
+}
+
+func main()
+{
+    mm_vadd();
     return 0;
 }
 // dotnet ../Epsilon/bin/Debug/net8.0/Epsilon.dll -o ./singlecycle/test/Generated/test -dump -sim ./singlecycle/test/test.e
