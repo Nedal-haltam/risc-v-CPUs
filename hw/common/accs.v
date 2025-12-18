30dcf692e8bc4a44c72032db1019c9cd4615ecf6
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-12-18 22:21:03 +0300

    add 3d convolution systolic design

--- hw/common/accs.v
+++ hw/common/accs.v
@@ -543,3 +543,170 @@ module SystolicArray_conv2d
 
 endmodule
 ////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+module PE_conv3d
+#(
+parameter PECount   = 0
+,parameter DataWidth = 32
+)
+(
+    input clk, rst, trigger, input `BIT_WIDTH file
+    ,input      `BIT_WIDTH InDim0Lane0
+    ,output reg `BIT_WIDTH OutDim0Lane0
+    ,input      `BIT_WIDTH InDim1Lane0
+    ,output reg `BIT_WIDTH OutDim1Lane0
+    ,input      `BIT_WIDTH InDim2Lane0
+    ,output reg `BIT_WIDTH OutDim2Lane0
+    ,output reg `BIT_WIDTH PEValue
+    ,input InternalRegisterEnable
+    ,input `BIT_WIDTH InternalRegisterInputValue0
+);
+    reg `BIT_WIDTH InternalRegister0;
+    always@(posedge clk) begin
+        if (InternalRegisterEnable) begin
+            InternalRegister0 <= InternalRegisterInputValue0;
+        end
+    end
+    always@(posedge clk) begin
+        if (rst) begin
+            OutDim0Lane0 <= 0;
+            OutDim1Lane0 <= 0;
+            OutDim2Lane0 <= 0;
+            PEValue <= 0;
+        end
+        else if (trigger) begin
+            OutDim0Lane0 <= InDim0Lane0;
+            OutDim1Lane0 <= InDim0Lane0 * InternalRegister0;
+        end
+    end
+endmodule
+module SystolicArray_conv3d
+(
+    input clk, rst, trigger
+    ,input `BIT_WIDTH Dim0Input00Lane0
+    ,input `BIT_WIDTH Dim0Input01Lane0
+    ,input `BIT_WIDTH Dim0Input02Lane0
+    ,input `BIT_WIDTH Dim0Input10Lane0
+    ,input `BIT_WIDTH Dim0Input11Lane0
+    ,input `BIT_WIDTH Dim0Input12Lane0
+    ,input `BIT_WIDTH Dim0Input20Lane0
+    ,input `BIT_WIDTH Dim0Input21Lane0
+    ,input `BIT_WIDTH Dim0Input22Lane0
+    ,input `BIT_WIDTH Dim1Input00Lane0
+    ,input `BIT_WIDTH Dim1Input01Lane0
+    ,input `BIT_WIDTH Dim1Input02Lane0
+    ,input `BIT_WIDTH Dim1Input10Lane0
+    ,input `BIT_WIDTH Dim1Input11Lane0
+    ,input `BIT_WIDTH Dim1Input12Lane0
+    ,input `BIT_WIDTH Dim1Input20Lane0
+    ,input `BIT_WIDTH Dim1Input21Lane0
+    ,input `BIT_WIDTH Dim1Input22Lane0
+    ,input `BIT_WIDTH Dim2Input00Lane0
+    ,input `BIT_WIDTH Dim2Input01Lane0
+    ,input `BIT_WIDTH Dim2Input02Lane0
+    ,input `BIT_WIDTH Dim2Input10Lane0
+    ,input `BIT_WIDTH Dim2Input11Lane0
+    ,input `BIT_WIDTH Dim2Input12Lane0
+    ,input `BIT_WIDTH Dim2Input20Lane0
+    ,input `BIT_WIDTH Dim2Input21Lane0
+    ,input `BIT_WIDTH Dim2Input22Lane0
+
+    ,input `BIT_WIDTH InternalRegisterEnableIndex
+    ,input `BIT_WIDTH InternalRegisterInputValue0
+
+    ,output `BIT_WIDTH Output0
+
+);
+    integer file;
+    wire `BIT_WIDTH PassThroughWires0Lane0[2:0][2:0];
+    wire `BIT_WIDTH PassThroughWires1Lane0[2:0][2:0];
+    wire `BIT_WIDTH PassThroughWires2Lane0[2:0][2:0];
+    assign PassThroughWires0Lane0[0][0] = Dim0Input00Lane0;
+    assign PassThroughWires0Lane0[0][1] = Dim0Input01Lane0;
+    assign PassThroughWires0Lane0[0][2] = Dim0Input02Lane0;
+    assign PassThroughWires0Lane0[1][0] = Dim0Input10Lane0;
+    assign PassThroughWires0Lane0[1][1] = Dim0Input11Lane0;
+    assign PassThroughWires0Lane0[1][2] = Dim0Input12Lane0;
+    assign PassThroughWires0Lane0[2][0] = Dim0Input20Lane0;
+    assign PassThroughWires0Lane0[2][1] = Dim0Input21Lane0;
+    assign PassThroughWires0Lane0[2][2] = Dim0Input22Lane0;
+    assign PassThroughWires1Lane0[0][0] = Dim1Input00Lane0;
+    assign PassThroughWires1Lane0[0][1] = Dim1Input01Lane0;
+    assign PassThroughWires1Lane0[0][2] = Dim1Input02Lane0;
+    assign PassThroughWires1Lane0[1][0] = Dim1Input10Lane0;
+    assign PassThroughWires1Lane0[1][1] = Dim1Input11Lane0;
+    assign PassThroughWires1Lane0[1][2] = Dim1Input12Lane0;
+    assign PassThroughWires1Lane0[2][0] = Dim1Input20Lane0;
+    assign PassThroughWires1Lane0[2][1] = Dim1Input21Lane0;
+    assign PassThroughWires1Lane0[2][2] = Dim1Input22Lane0;
+    assign PassThroughWires2Lane0[0][0] = Dim2Input00Lane0;
+    assign PassThroughWires2Lane0[0][1] = Dim2Input01Lane0;
+    assign PassThroughWires2Lane0[0][2] = Dim2Input02Lane0;
+    assign PassThroughWires2Lane0[1][0] = Dim2Input10Lane0;
+    assign PassThroughWires2Lane0[1][1] = Dim2Input11Lane0;
+    assign PassThroughWires2Lane0[1][2] = Dim2Input12Lane0;
+    assign PassThroughWires2Lane0[2][0] = Dim2Input20Lane0;
+    assign PassThroughWires2Lane0[2][1] = Dim2Input21Lane0;
+    assign PassThroughWires2Lane0[2][2] = Dim2Input22Lane0;
+    wire `BIT_WIDTH PEOutDim0Lane0 [2:0][2:0][2:0];
+    wire `BIT_WIDTH PEOutDim1Lane0 [2:0][2:0][2:0];
+    wire `BIT_WIDTH PEOutDim2Lane0 [2:0][2:0][2:0];
+    wire `BIT_WIDTH PEValues [26:0];
+    genvar Dim0Index, Dim1Index, Dim2Index, DummyIndex;
+    generate
+	    for (Dim0Index = 0; Dim0Index < 3; Dim0Index = Dim0Index + 1) begin : Dim0IndexForLoopBlock
+		    for (Dim1Index = 0; Dim1Index < 3; Dim1Index = Dim1Index + 1) begin : Dim1IndexForLoopBlock
+			    for (Dim2Index = 0; Dim2Index < 3; Dim2Index = Dim2Index + 1) begin : Dim2IndexForLoopBlock
+					localparam PECount = Dim0Index * 3 * 3 * 1 + Dim1Index * 3 * 1 + Dim2Index * 1 + 0;
+					wire `BIT_WIDTH InDim0Lane0;
+					if (Dim0Index == 0) begin
+					    assign InDim0Lane0 = PassThroughWires0Lane0[Dim1Index][Dim2Index];
+					end
+					else begin
+					    assign InDim0Lane0 = PEOutDim0Lane0[Dim0Index-1][Dim1Index][Dim2Index];
+					end
+					wire `BIT_WIDTH InDim1Lane0;
+					assign InDim1Lane0 = PassThroughWires1Lane0[Dim0Index][Dim2Index];
+					wire `BIT_WIDTH InDim2Lane0;
+					assign InDim2Lane0 = PassThroughWires2Lane0[Dim0Index][Dim1Index];
+					PE_conv3d #(.PECount(PECount)) pe
+					(
+					    .clk(clk),
+					    .rst(rst),
+					    .trigger(trigger),
+					    .file(1),
+					    .InDim0Lane0(InDim0Lane0),
+					    .OutDim0Lane0(PEOutDim0Lane0[Dim0Index][Dim1Index][Dim2Index]),
+					    .InDim1Lane0(InDim1Lane0),
+					    .OutDim1Lane0(PEOutDim1Lane0[Dim0Index][Dim1Index][Dim2Index]),
+					    .InDim2Lane0(InDim2Lane0),
+					    .OutDim2Lane0(PEOutDim2Lane0[Dim0Index][Dim1Index][Dim2Index]),
+					    .PEValue(PEValues[PECount])
+					    ,.InternalRegisterEnable(InternalRegisterEnableIndex == PECount+1)
+					    ,.InternalRegisterInputValue0(InternalRegisterInputValue0)
+					);
+			    end
+		    end
+	    end
+    endgenerate
+    wire signed `BIT_WIDTH SUM0 =
+        $signed(PEOutDim1Lane0[0][0][0]) + $signed(PEOutDim1Lane0[0][0][1]) + $signed(PEOutDim1Lane0[0][0][2]) + $signed(PEOutDim1Lane0[0][1][0]) + $signed(PEOutDim1Lane0[0][1][1]) + $signed(PEOutDim1Lane0[0][1][2]) + $signed(PEOutDim1Lane0[0][2][0]) + $signed(PEOutDim1Lane0[0][2][1]) + $signed(PEOutDim1Lane0[0][2][2]) + $signed(PEOutDim1Lane0[1][0][0]) + $signed(PEOutDim1Lane0[1][0][1]) + $signed(PEOutDim1Lane0[1][0][2]) + $signed(PEOutDim1Lane0[1][1][0]) + $signed(PEOutDim1Lane0[1][1][1]) + $signed(PEOutDim1Lane0[1][1][2]) + $signed(PEOutDim1Lane0[1][2][0]) + $signed(PEOutDim1Lane0[1][2][1]) + $signed(PEOutDim1Lane0[1][2][2]) + $signed(PEOutDim1Lane0[2][0][0]) + $signed(PEOutDim1Lane0[2][0][1]) + $signed(PEOutDim1Lane0[2][0][2]) + $signed(PEOutDim1Lane0[2][1][0]) + $signed(PEOutDim1Lane0[2][1][1]) + $signed(PEOutDim1Lane0[2][1][2]) + $signed(PEOutDim1Lane0[2][2][0]) + $signed(PEOutDim1Lane0[2][2][1]) + $signed(PEOutDim1Lane0[2][2][2]) +  0;
+    function [7:0] clamp8(input signed `BIT_WIDTH v);
+        reg signed `BIT_WIDTH abs_v;
+        begin
+            abs_v = (v < 0) ? -v : v;
+            if (abs_v < 0) // keep this in case we removed the abs
+                clamp8 = 8'd0;
+            else if (abs_v > 8'd255)
+                clamp8 = 8'd255;
+            else
+                clamp8 = abs_v[7:0];
+        end
+    endfunction
+
+    // wire `BIT_WIDTH c0 = SUM0;
+    assign Output0 = SUM0;
+
+
+endmodule
+////////////////////////////////////////////////////////////////////////////////////
